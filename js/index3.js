555b42aaade8af6541d5d0f6c990ea0b3c619cff
Ewerton Oliveira <ewertsoliveira@gmail.com>
2026-09-10 16:26:08 -0300

    feat(media): migra todas as imagens para formato WebP otimizado

--- js/index3.js
+++ js/index3.js
@@ -99,7 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const FRAME_COUNT = 160;
     const FOLDER_PATH = "assets/sequencia/";
     const FRAME_NAME = "Sequência 01";
-    const FRAME_EXT = ".jpg";
+    const FRAME_EXT = ".webp";
 
     // Resolução base que o vídeo original tinha
     canvas.width = 1920;
